8637603f36da48358b578c8bea90bae61667ee18
luanneves07 <luannevessilva@gmail.com>
2019-10-29 00:14:57 -0300

    GYMP-19001 > Prepara configurações de regras de código
    - editorconfig
    - eslintrc
    - prettierrc
    - Integração de prettier com Eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  /**
+   * Adiciona regras de exceção para o projeto. Pode ser criado junto com o time
+   * de dev.
+   * Não necessita de "this" obrigatoriamente nos métodos de classe
+   */
+  rules: {
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+  },
+};
