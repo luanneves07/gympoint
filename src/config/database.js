ee1305a87ff70c9849d8fc8abb9fd1a5d6a6cf51
luanneves07 <luannevessilva@gmail.com>
2019-10-29 01:02:29 -0300

    GYMP-19001 > Prepara sequelize
    - Faz a configuração dos diretórios da seguinte forma:
    	src+
    	|_app+
    		|_controllers+
    		|_models+
    	|_config+
    		|_databse.js
    	|_database+
    		|_databse+
    			|_migrations+
    		|_seeds+
    	|_app.js
    	|_routes.js
    	|_server.js
    	.arquivos_de_config_geral
    - Instala o sequelize e o sequelize-cli
    - Configura o sequelizerc para exportar o caminho ate os diretorios criados
    - Adiciona configurações do banco de dados dentro do src/config/database.js

---
+++ src/config/database.js
@@ -0,0 +1,17 @@
+/**
+ * A sintaxe de import/export é feita da maneira antiga pois a interface do
+ * terminal para o sequelize-cli ainda não consegue identificar a nova
+ * terminologia.
+ */
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true, // Garante as colunas createdAt e updatedAt automaticamente
+    underscored: true, // Pardoniza a nemenclatura das tabelas com _ no lugar de camelCase
+    underscoredAll: true,
+  },
+};
