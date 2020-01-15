a2a17ca91dd614382504d67fdea6e1864ef049c2
luanneves07 <luannevessilva@gmail.com>
2020-01-15 19:34:53 -0300

    GYMP-20001 - Gestao de matriculas
    - Criacao de metodo post
    - Criacao de metodo delete
    - Criacao de metodo put
    - Validacoes apos update

--- src/routes.js
+++ src/routes.js
@@ -33,5 +33,8 @@ routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.delete);
 
 routes.get('/registrations', RegistrationController.index);
+routes.post('/registrations', RegistrationController.store);
+routes.put('/registrations/:id', RegistrationController.update);
+routes.delete('/registrations/:id', RegistrationController.delete);
 
 export default routes;
